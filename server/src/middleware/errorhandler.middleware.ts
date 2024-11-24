04470181d5c51a1afd76987d165a903fe3af5222
Aayush Bhattarai <aayush.bhattarai.np@gmail.com>
2024-11-24 22:20:04 +0545

    completed error handler middleware

---
+++ server/src/middleware/errorhandler.middleware.ts
@@ -0,0 +1,26 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { DotenvConfig } from "../config/env.config";
+
+export const errorHandler = (
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log("🚀 ~ errorHandler ~ error:", error);
+  let statusCode = 500;
+  let data = {
+    success: false,
+    message: "error",
+    ...(DotenvConfig.DEBUG_MODE === "true" && { original: error.message }),
+  };
+
+  if (error?.isOperational || error?.isCustom) {
+    statusCode = error.statusCode;
+    data = {
+      ...data,
+      message: error.message,
+    };
+  }
+  return res.status(statusCode).json(data.message);
+};
