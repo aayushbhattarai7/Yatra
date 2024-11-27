a413105ebd8f516228b704bcb4a7ba61303877be
Aayush Bhattarai <aayush.bhattarai.np@gmail.com>
2024-11-27 21:47:24 +0545

    Refactor: Migrate form handling to react-hook-form

--- client/src/ui/common/organisms/Register.tsx
+++ client/src/ui/common/organisms/Register.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import { gql, useMutation } from "@apollo/client";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const SIGNUP_MUTATION = gql`
   mutation Signup($data: UserDTO!) {
@@ -12,62 +12,40 @@ const SIGNUP_MUTATION = gql`
   }
 `;
 
+interface FormData {
+  email: string;
+  password: string;
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  phoneNumber: string;
+  role: "GUIDE" | "USER" | "ADMIN";
+  gender: "MALE" | "FEMALE" | "OTHER";
+}
+
 const UserRegister = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    firstName: "",
-    middleName: "",
-    lastName: "",
-    phoneNumber: "",
-    role: "GUIDE", 
-    gender: "MALE", 
+  const { register, handleSubmit, reset } = useForm<FormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+      firstName: "",
+      middleName: "",
+      lastName: "",
+      phoneNumber: "",
+      role: "GUIDE",
+      gender: "MALE",
+    },
   });
 
   const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION);
 
-  const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const { name, value } = event.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-
+  const onSubmit: SubmitHandler<FormData> = async (formData) => {
     try {
-      const {
-        email,
-        password,
-        firstName,
-        middleName,
-        lastName,
-        phoneNumber,
-        role,
-        gender,
-      } = formData;
-
-      await signup({
-        variables: {
-          data: {
-            email,
-            password,
-            firstName,
-            middleName,
-            lastName,
-            phoneNumber,
-            role,
-            gender,
-          },
-        },
-      });
+      await signup({ variables: { data: formData } });
 
       if (data) {
         alert(`Signup successful! Welcome, ${data.signup.firstName}!`);
+        reset(); 
       }
     } catch (err) {
       console.error("Error signing up:", err);
@@ -75,16 +53,13 @@ const UserRegister = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>
           Email:
           <input
             type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            required
+            {...register("email", { required: "Email is required" })}
           />
         </label>
       </div>
@@ -93,10 +68,7 @@ const UserRegister = () => {
           Password:
           <input
             type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-            required
+            {...register("password", { required: "Password is required" })}
           />
         </label>
       </div>
@@ -105,22 +77,14 @@ const UserRegister = () => {
           First Name:
           <input
             type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleInputChange}
-            required
+            {...register("firstName", { required: "First Name is required" })}
           />
         </label>
       </div>
       <div>
         <label>
           Middle Name:
-          <input
-            type="text"
-            name="middleName"
-            value={formData.middleName}
-            onChange={handleInputChange}
-          />
+          <input type="text" {...register("middleName")} />
         </label>
       </div>
       <div>
@@ -128,10 +92,7 @@ const UserRegister = () => {
           Last Name:
           <input
             type="text"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleInputChange}
-            required
+            {...register("lastName", { required: "Last Name is required" })}
           />
         </label>
       </div>
@@ -140,22 +101,16 @@ const UserRegister = () => {
           Phone Number:
           <input
             type="tel"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={handleInputChange}
-            required
+            {...register("phoneNumber", {
+              required: "Phone Number is required",
+            })}
           />
         </label>
       </div>
       <div>
         <label>
           Role:
-          <select
-            name="role"
-            value={formData.role}
-            onChange={handleInputChange}
-            required
-          >
+          <select {...register("role", { required: "Role is required" })}>
             <option value="GUIDE">Guide</option>
             <option value="USER">User</option>
             <option value="ADMIN">Admin</option>
@@ -165,12 +120,7 @@ const UserRegister = () => {
       <div>
         <label>
           Gender:
-          <select
-            name="gender"
-            value={formData.gender}
-            onChange={handleInputChange}
-            required
-          >
+          <select {...register("gender", { required: "Gender is required" })}>
             <option value="MALE">Male</option>
             <option value="FEMALE">Female</option>
             <option value="OTHER">Other</option>
