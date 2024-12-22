0e4b5b0d22d05fcda533c1ef3b9d814d77be8ba7
Aayush Bhattarai <aayush.bhattarai.np@gmail.com>
2024-12-22 20:39:22 +0545

    fix typeDfs

---
+++ server/src/graphql/schema/typeDefs.ts
@@ -0,0 +1,24 @@
+import { gql } from "apollo-server-express"
+
+export const typeDefs = gql`
+
+type User{
+id:ID!
+firstName: String!
+middleName:String
+lastName: String!
+email: String!
+role: Enum!
+phoneNumber: String!
+gender: Enum!
+password: String!
+
+
+
+}
+type Mutation {
+  register(firstName: String!, middleName:String, lastName:String!, email:String!, 
+  role: Enum!, phoneNumber: String!, gender: Enum!, password: String! ): User
+  login(email:String!, password:String!):User
+}
+`
