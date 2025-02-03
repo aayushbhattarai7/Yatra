1d4ee2ab5d9099724ea34779f89311c4b85f49ff
aayushbhattarai7 <aayush.bhattarai.np@gmail.com>
2025-02-03 23:03:06 +0545

    completed displaying user profile

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -169,12 +169,11 @@ const Navbar = () => {
                 <div className="flex items-center px-5">
                   <img
                     className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src=""
                     alt="Profile"
                   />
                   <div className="ml-3">
                     <div className="text-base font-medium text-gray-800">
-                      Jon Doe
                     </div>
                   </div>
                 </div>
@@ -213,7 +212,7 @@ const PopupButton = ({
       {profileImage ? (
         <img
           className="h-8 w-8 rounded-full"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          src=""
           alt="Profile"
         />
       ) : (
