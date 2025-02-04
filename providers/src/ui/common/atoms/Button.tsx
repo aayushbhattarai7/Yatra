ae517b9be22d9fb124fb41396a9f2580942b95ec
aayushbhattarai7 <aayush.bhattarai.np@gmail.com>
2025-02-04 08:10:30 +0545

    setup app for service providers(travel and guides)

---
+++ providers/src/ui/common/atoms/Button.tsx
@@ -0,0 +1,34 @@
+import {image} from '../../../config/constant/image'
+interface ButtonProps {
+  name?: string;
+  type: "submit" | "reset" | "button";
+  disabled?: boolean;
+  buttonText: string;
+  className?: string;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  name,
+  type = undefined,
+  disabled = false,
+  buttonText,
+  className,
+  onClick,
+}) => {
+  return (
+    <div>
+      <button
+        type={type}
+        name={name}
+        disabled={disabled}
+        className={` w-32 mb-4 bg-[#1366D9] text-white hover:bg-blue-700 p-3 rounded-xl font-poppins shadow-xl ${className}`}
+        onClick={onClick}
+      >
+        {disabled ? <image /> : buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ providers/src/ui/common/atoms/InputField.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { FieldError, UseFormRegister } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+interface InputFieldProps {
+  placeholder?: string;
+  type?: string;
+  name: string;
+  readOnly?: boolean;
+  error?: FieldError;
+  register: UseFormRegister<any>;
+  multiple?: boolean;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  className: string;
+  required?: boolean;
+  maxLength?: number;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  accept?: string;
+  value?: string;
+  icon?: React.ReactNode;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+  placeholder,
+  type = "text",
+  name,
+  readOnly,
+  error,
+  register,
+  multiple,
+  onChange,
+  className,
+  required,
+  accept,
+  value,
+  icon,
+}) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const toggleField = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <div className="flex relative items-center">
+      {icon && (
+        <span className="absolute left-3 text-black text-xl">{icon}</span>
+      )}
+      <input
+        type={type === "password" ? (showPassword ? "text" : "password") : type}
+        readOnly={readOnly}
+        placeholder={placeholder}
+        multiple={multiple}
+        accept={accept}
+        {...register(name, { required })}
+        onChange={onChange}
+        className={`w-[364px] h-[52px] rounded-lg p-5 pl-${
+          icon ? "10" : "5"
+        } border border-black bg-[#F0EDFF] outline-none placeholder-black ${className}`}
+      />
+      {type === "password" && (
+        <span
+          className="absolute right-5 top-1/3 transform-translate-y-1/2 cursor-pointer"
+          onClick={toggleField}
+        >
+          {showPassword ? <FaEye /> : <FaEyeSlash />}
+        </span>
+      )}
+      {error && <p className="text-red-500 text-xs mt-1">{error.message}</p>}
+    </div>
+  );
+};
+
+export default InputField;
