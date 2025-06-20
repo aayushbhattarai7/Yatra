6452e5eddd904a90b27854ae9df8bc1a99166d22
Aayush Bhattarai <aayush.bhattarai.np@gmail.com>
2025-06-20 13:37:31 +0545

    Update database.config.ts

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -1,6 +1,6 @@
-// import "reflect-metadata";
-// import { DataSource } from "typeorm";
-// import { DotenvConfig } from "./env.config";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { DotenvConfig } from "./env.config";
 // export const AppDataSource = new DataSource({
 //   type: "postgres",
 //   host: DotenvConfig.DATABASE_HOST,
